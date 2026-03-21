e24ccc678826099e557461bc25970e9c0c87c7d4
Sophea <sopheaisanidiot@gmail.com>
2026-03-21 18:08:50 +0700

    setup

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+
+$admin_pages = ['admin_dashboard'];
+$non_logged_pages = ['login', 'register'];
+$main_pages = ['dashboard'];
+$logged_pages = ['profile', 'logout'];
+$array_pages = [...$non_logged_pages, ...$logged_pages, ...$admin_pages, ...$main_pages];
+
+$page = $_GET['page'];
+if(!in_array($page, $non_logged_pages)){
+    include 'includes/header.php';
+    include 'includes/novbar.php';
+}
+if(in_array($page, $array_pages)){
+    include './pages/' . $page . '.php';
+} else{
+    header('Location: ./?page=dashboard');
+}
+
+
+
+include 'includes/footer.php';
+?>
